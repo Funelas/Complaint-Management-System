b3482c95043779746e2838d50ccc59c8e5411e91
Allan John Funelas <funelasallanjohn@gmail.com>
2025-03-29 19:36:14 +0800

    Converting User Form Interface into a Class

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+import sqlite3
+import tkinter as tk
 from PIL import Image
 
 # System Defaults
@@ -9,13 +11,147 @@ profile_img = Image.open("assets/images/profile.png")
 root = ctk.CTk()
 root.geometry("1000x600")
 root.title("Complaint Management System")
+connection = sqlite3.connect("cms.db")
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS complaint_form (
+               complaint_no INTEGER PRIMARY KEY AUTOINCREMENT,
+               name VARCHAR(250),
+               user_id VARCHAR(250),
+               gender VARCHAR(250),
+               Subject VARCHAR(250),
+               Complaint TEXT
+               )''')
+class ComplaintForm:
+    def __init__(self, root, cursor, connection):
+        self.root = root
+        self.cursor = cursor
+        self.connection = connection
+        self.user_comp_int_frm = ctk.CTkFrame(self.root, fg_color="transparent")
+        self.username_com_form = tk.StringVar()
+        self.userid_com_form = tk.StringVar()
+        self.usergender_com_form = tk.StringVar()
+        self.subject_com_form = tk.StringVar()
+         # Frame for the entire form
+        self.header_frm = ctk.CTkFrame(self.user_comp_int_frm, fg_color= "transparent")
+        self.logout_btn = ctk.CTkButton(self.header_frm, text= "Logout", command = lambda: log_in("logout", "logout"))
+        self.user_comp_title_lbl = ctk.CTkLabel(self.header_frm, text="Complaint Form", font=("Poppins", 35))
+        self.user_comp_form_frm = ctk.CTkScrollableFrame(self.user_comp_int_frm, border_color="#00009e", border_width=2, width=800, height=500)
 
+        # Name and ID Frame
+        self.name_id_frm = ctk.CTkFrame(self.user_comp_form_frm, fg_color="transparent")
+        self.user_name_frm = ctk.CTkFrame(self.name_id_frm, fg_color="transparent")
+        self.user_name_lbl = ctk.CTkLabel(self.user_name_frm, text="Name:", font=("Poppins", 15))
+        self.user_name_ent = ctk.CTkEntry(self.user_name_frm, font=("Poppins", 15), placeholder_text="Enter your name", width=250, textvariable=self.username_com_form)
+        
+        self.user_id_frm = ctk.CTkFrame(self.name_id_frm, fg_color="transparent")
+        self.user_id_lbl = ctk.CTkLabel(self.user_id_frm, text="ID Number:", font=("Poppins", 15))
+        self.user_id_ent = ctk.CTkEntry(self.user_id_frm, font=("Poppins", 15), placeholder_text="Enter your ID", width=250, textvariable=self.userid_com_form)
+
+        # Gender Frame
+        self.gender_frm = ctk.CTkFrame(self.user_comp_form_frm, fg_color="transparent")
+        self.gender_values = ["Male", "Female", "I prefer not to say..."]
+        self.gender_lbl = ctk.CTkLabel(self.gender_frm, text="Gender:", font=("Poppins", 15))
+        self.gender_dropdown = ctk.CTkComboBox(self.gender_frm, width=250, values=self.gender_values, font=("Poppins", 15), variable=self.usergender_com_form)
+
+        # Subject Frame
+        self.subject_frm = ctk.CTkFrame(self.user_comp_form_frm, fg_color="transparent")
+        self.subject_lbl = ctk.CTkLabel(self.subject_frm, text="Subject:", font=("Poppins", 15))
+        self.subject_ent = ctk.CTkEntry(self.subject_frm, font=("Poppins", 15), placeholder_text="Subject of Complaint", width=500, textvariable=self.subject_com_form)
+
+        # Complaint Frame
+        self.com_form_frm = ctk.CTkFrame(self.user_comp_form_frm, fg_color="transparent")
+        self.com_form_lbl = ctk.CTkLabel(self.com_form_frm, text="Complaint:", font=("Poppins", 15))
+        self.com_form_ent = ctk.CTkTextbox(self.user_comp_form_frm, font=("Poppins", 15), width=700, height=150)
+
+        # Submit Button
+    def show(self):
+        self.hide()
+        self.submit_com_form_btn = ctk.CTkButton(self.user_comp_form_frm, text="Submit Now", font=("Poppins", 15), command=self.insert)
+
+        # Packing Widgets
+        self.user_name_lbl.pack(padx=(50, 3), pady=(10, 0), side="left")
+        self.user_name_ent.pack(padx=(10, 20), pady=(10, 0), side="left")
+        self.user_id_lbl.pack(padx=(50, 3), pady=(10, 0), side="left")
+        self.user_id_ent.pack(padx=(10, 20), pady=(10, 0), side="left")
+
+        self.gender_lbl.pack(padx=(50, 3), pady=(10, 10), side="left")
+        self.gender_dropdown.pack(pady=(0, 3), side="left")
+
+        self.subject_lbl.pack(padx=(50, 3), pady=(15, 10), side="left")
+        self.subject_ent.pack(padx=(10, 20), pady=(15, 10), side="left")
+
+        self.com_form_lbl.pack(padx=(50, 3), pady=5, side="left")
+
+        self.user_name_frm.pack(padx=10, pady=1, side="left")
+        self.user_id_frm.pack(padx=10, pady=1, side="left")
+
+        self.name_id_frm.pack(pady=20, padx=5)
+        self.gender_frm.pack(pady=10, padx=15, fill="x", expand=True)
+        self.subject_frm.pack(pady=10, padx=15, fill="x", expand=True)
+        self.com_form_frm.pack(pady=3, padx=15, fill="x", expand=True)
+        self.com_form_ent.pack(padx=(65, 20), pady=5)
+        self.submit_com_form_btn.pack(pady=5)
+        self.logout_btn.pack(padx = (20, 125), side= "left")
+        self.user_comp_title_lbl.pack(side = "left")
+        
+        self.header_frm.pack(pady = 15, fill= "x", expand = "True")
+        self.user_comp_form_frm.pack(padx=20, pady=5)
+
+
+       
+        # Pack the main frame
+        self.user_comp_int_frm.pack()
+
+    def insert(self):
+        """Inserts the form data into the database."""
+        name = self.username_com_form.get()
+        user_id = self.userid_com_form.get()
+        gender = self.usergender_com_form.get()
+        subject = self.subject_com_form.get()
+        complain = self.com_form_ent.get("0.0", "end-1c")
+
+        print(complain)
+        self.cursor.execute(
+            "INSERT INTO complaint_form (name, user_id, gender, Subject, Complaint) VALUES (?, ?, ?, ?, ?)",
+            (name, user_id, gender, subject, complain)
+        )
+        self.connection.commit()
+        self.cursor.execute("SELECT * FROM complaint_form")
+        records = self.cursor.fetchall()
+        print(records)
+        self.show()
+    
+    def hide(self):
+        if hasattr(self, 'gender_dropdown'):
+            self.gender_dropdown.set("")
+        if hasattr(self, 'subject_ent'):
+            self.subject_ent.delete(0, "end")
+        if hasattr(self, 'com_form_ent'):
+            self.com_form_ent.delete("0.0", "end")
+        if hasattr(self, 'user_name_ent'):
+            self.user_name_ent.delete(0, "end")
+        if hasattr(self, 'user_id_ent'):
+            self.user_id_ent.delete(0, "end")
+        self.user_comp_int_frm.forget()
+
+complaintforminterface = ComplaintForm(root, cursor, connection)
 # Functions
-def log_in(account_type):
-    print("Function Ran")
-    if account_type == 0:
+def log_in(username, userpassword):
+    global cursor, complaintforminterface
+    cursor.execute("SELECT * FROM complaint_form")
+    records = cursor.fetchall()
+    print(records)
+    if username == userpassword == "logout":
+        login_frm.pack()
+        complaintforminterface.hide()
+    elif username.get() == userpassword.get() == "user":
         login_frm.forget()
-        user_comp_int_frm.pack()
+        complaintforminterface.show()
+    elif username.get() == userpassword.get() == "admin":
+        login_frm.forget()
+        admin_main_int.pack()
+
+
 
 # Log in Interface
 login_frm = ctk.CTkFrame(root, fg_color= "transparent")
@@ -25,17 +161,20 @@ login_title_frm.pack(pady = (50, 0))
 login_title_lbl.pack(padx = 20, pady = 40)
 
 # Start of User Form #
-
+## Variable Holders ##
+username_login = tk.StringVar()
+userpassword_login = tk.StringVar()
+## End of Variable Holder ##
 user_form_frm = ctk.CTkFrame(login_frm, border_color= "#00009e", corner_radius= 20, border_width= 3)
 icon_frm = ctk.CTkFrame(user_form_frm)
 user_form_icon = ctk.CTkImage(profile_img, size=(80,80))
 user_form_icon_holder = ctk.CTkLabel(user_form_frm, image= user_form_icon, text= "", width= 250)
 user_form_lbl = ctk.CTkLabel(user_form_frm, text= "Member Login" , font= ("Poppins", 15), text_color= "#00009e", anchor= "w", width= 250)
 user_username_lbl = ctk.CTkLabel(user_form_frm, text= "Username:" , font= ("Poppins", 15), text_color= "#00009e", anchor= "w", width= 250)
-user_username_ent = ctk.CTkEntry(user_form_frm, placeholder_text="Enter your username:", corner_radius= 50, width= 250, font= ("Poppins", 12))
+user_username_ent = ctk.CTkEntry(user_form_frm, placeholder_text="Enter your username:", corner_radius= 50, width= 250, font= ("Poppins", 12), textvariable= username_login)
 user_password_lbl = ctk.CTkLabel(user_form_frm, text= "Password:", font= ("Poppins", 15), text_color= "#00009e", anchor= "w", width= 250)
-user_password_ent = ctk.CTkEntry(user_form_frm, placeholder_text="Enter your password:", show= "•", corner_radius= 50, width= 250, font= ("Poppins", 12))
-user_form_submit_btn = ctk.CTkButton(user_form_frm, text= "Log in", font= ("Poppins", 15), width= 250, corner_radius= 50, command= lambda: log_in(0))
+user_password_ent = ctk.CTkEntry(user_form_frm, placeholder_text="Enter your password:", show= "•", corner_radius= 50, width= 250, font= ("Poppins", 12), textvariable= userpassword_login)
+user_form_submit_btn = ctk.CTkButton(user_form_frm, text= "Log in", font= ("Poppins", 15), width= 250, corner_radius= 50, command= lambda: log_in(username_login, userpassword_login))
 
 user_form_icon_holder.pack(pady= (5,0), padx= 20)
 user_form_lbl.configure(anchor="center", justify="center")
@@ -50,57 +189,14 @@ login_frm.pack()
 # End of User Form #
 
 # Start of User Complaint Interface #
-user_comp_int_frm = ctk.CTkFrame(root, fg_color= "transparent")
-user_comp_title_lbl = ctk.CTkLabel(user_comp_int_frm, text= "Complaint Form", font= ("Poppins", 35))
-user_comp_form_frm = ctk.CTkScrollableFrame(user_comp_int_frm, border_color= "#00009e", border_width= 2, width = 800, height = 500)
 
-name_id_frm = ctk.CTkFrame(user_comp_form_frm, fg_color= "transparent")
-user_name_frm = ctk.CTkFrame(name_id_frm, fg_color= "transparent")
-user_name_lbl = ctk.CTkLabel(user_name_frm, text= "Name: ", font= ("Poppins", 15))
-user_name_ent = ctk.CTkEntry(user_name_frm, font= ("Poppins", 15), placeholder_text= "Enter your name", width= 250)
-user_id_frm = ctk.CTkFrame(name_id_frm, fg_color= "transparent")
-user_id_lbl = ctk.CTkLabel(user_id_frm, text= "ID Number: ", font= ("Poppins", 15))
-user_id_ent = ctk.CTkEntry(user_id_frm, font= ("Poppins", 15), placeholder_text= "Enter your ID", width= 250)
 
-gender_frm = ctk.CTkFrame(user_comp_form_frm, fg_color= "transparent")
-gender_values = ["Male", "Female", "I prefer not to say..."]
-gender_lbl = ctk.CTkLabel(gender_frm, text= "Gender: ", font= ("Poppins", 15))
-gender_dropdown = ctk.CTkComboBox(gender_frm, width = 250, values= gender_values, font= ("Poppins", 15))
+# Start of Admin Interface #
+admin_main_int = ctk.CTkFrame(root, fg_color= "transparent")
+user_comp_title_lbl = ctk.CTkLabel(admin_main_int, text= "Complaint Overview", font= ("Poppins", 35))
+complaints_frm = ctk.CTkScrollableFrame(admin_main_int, border_color="#00009e", width= 800)
 
-subject_frm = ctk.CTkFrame(user_comp_form_frm, fg_color= "transparent")
-subject_lbl = ctk.CTkLabel(subject_frm, text= "Subject: ", font= ("Poppins", 15))
-subject_ent = ctk.CTkEntry(subject_frm, font= ("Poppins", 15), placeholder_text= "Subject of Complaint", width= 500)
-
-com_form_frm = ctk.CTkFrame(user_comp_form_frm, fg_color= "transparent")
-com_form_lbl = ctk.CTkLabel(com_form_frm, text= "Complaint: ", font= ("Poppins", 15))
-com_form_ent = ctk.CTkTextbox(user_comp_form_frm, font= ("Poppins", 15), width= 700, height = 150)
-
-submit_com_form_btn = ctk.CTkButton(user_comp_form_frm, text= "Submit Now", font= ("Poppins", 15))
-user_name_lbl.pack(padx= (50, 3), pady= (10, 0),side= "left")
-user_name_ent.pack(padx= (10, 20), pady= (10, 0),side= "left")
-user_id_lbl.pack(padx= (50, 3), pady= (10, 0),side= "left")
-user_id_ent.pack(padx= (10, 20), pady= (10, 0),side= "left")
-
-gender_lbl.pack(padx= (50, 3), pady= (10, 10), side= "left")
-gender_dropdown.pack(pady = (0, 3), side = "left")
-
-subject_lbl.pack(padx= (50, 3), pady= (15, 10), side= "left")
-subject_ent.pack(padx= (10, 20), pady= (15, 10), side= "left")
-
-com_form_lbl.pack(padx= (50, 3), pady= 5, side= "left")
-
-user_name_frm.pack(padx = 10, pady = 1, side = "left")
-user_id_frm.pack(padx = 10, pady = 1, side = "left")
-
-name_id_frm.pack(pady= 20 , padx = 5)
-gender_frm.pack(pady= 10 , padx = 15, fill = "x", expand = True)
-subject_frm.pack(pady= 10 , padx = 15, fill = "x", expand = True)
-com_form_frm.pack(pady= 3 , padx = 15, fill = "x", expand = True)
-com_form_ent.pack(padx= (65, 20), pady= 5)
-submit_com_form_btn.pack(pady = 5)
 user_comp_title_lbl.pack(pady= 15)
-user_comp_form_frm.pack(padx = 20, pady = 5)
-
-
-# End of User Complaint Interface #
+complaints_frm.pack(fill = "x", expand = True)
+# End of Admin Interface #
 root.mainloop()
